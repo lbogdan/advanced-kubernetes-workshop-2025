1d6e6eca02d90ce05ebd860f8735b523cf1f4c30
Bogdan Luca <bogdan@together.ai>
2025-10-20 13:41:44 +0300

    task 7 - add ingress-nginx

--- workshop.sh
+++ workshop.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+CP_IP=''
+
 install_metrics_server() {
   repo_name='metrics-server'
   repo_url='https://kubernetes-sigs.github.io/metrics-server/'
@@ -14,4 +16,18 @@ install_metrics_server() {
   helm upgrade --install "$app_name" --namespace "$namespace" "$repo_name/$chart_name"
 }
 
-install_metrics_server
+install_ingress_nginx() {
+  repo_name='ingress-nginx'
+  repo_url='https://kubernetes.github.io/ingress-nginx'
+  app_name='ingress-nginx'
+  chart_name='ingress-nginx'
+  namespace='ingress-nginx'
+  helm repo add "$repo_name" "$repo_url"
+  helm repo update "$repo_name"
+  values="$(sed -e "s|\$CP_IP|$CP_IP|" manifests/helm/ingress-nginx-values.yaml)"
+  helm diff upgrade --color --install "$app_name" --namespace "$namespace" "$repo_name/$chart_name" --values <(echo "$values") | less -R
+  helm upgrade --create-namespace --install "$app_name" --namespace "$namespace" "$repo_name/$chart_name" --values <(echo "$values")
+}
+
+# install_metrics_server
+install_ingress_nginx
